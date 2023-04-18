f755b0cdf6bb9c2453063ca5b1736f9d79087c2e
Pablo Hernandez <pablo@Pablos-MacBook-Air.local>
2023-04-18 14:09:55 -0600

    first

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log("hello");
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetchCatImage()
+    btnClick()
+})
+
+function fetchCatImage() {
+    let image = document.getElementById("cat-image");
+    fetch("https://api.thecatapi.com/v1/images/search")
+    .then(resp => resp.json())
+    .then(json => image.src = json[0].url)
+
+}
+
+function btnClick() {
+    let button = document.getElementById("cat-btn");
+    button.addEventListener("click", fetchCatImage)
+}
